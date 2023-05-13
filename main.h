63b69d371fc98547c9f5a5761b68d99c9b9990c3
dekuu5 <deku332211@gmail.com>
2023-05-13 15:58:52 +0300

    bueled 2 fuctions

--- main.h
+++ main.h
@@ -6,9 +6,19 @@
 #define POLYNOMIALEXPRESSIONS_MAIN_H
 
 
-class main {
-
+struct node {
+    int coef;
+    int power;
+    node* next;
 };
 
+node * creatTerm(int coef,int pow);
+void insert_term(node** head, int coef, int pow);
+node* sum_polynomials(node* head1, node* head2);
+node* multiply_polynomials(node* head1, node* head2);
+int evaluate_polynomial(node* head, int x);
+
+
+void print_polynomial(struct Node* head);
 
 #endif //POLYNOMIALEXPRESSIONS_MAIN_H
--- node.cpp
+++ node.cpp
@@ -2,4 +2,24 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <stdlib.h
+#include <stdlib.h>
+
+
+node * creatTerm(int coef,int pow){
+    node * n = (node *)malloc(sizeof(node));
+    n->coef = coef;
+    n->power= pow;
+    n->next = nullptr;
+    return n;
+}
+
+void insertTerm(node** head, int coef, int pow){
+    node * term = creatTerm(coef,pow);
+    if (*head == nullptr){
+        (*head) = term;
+        return;
+    }
+    node *p = *head;
+    while (p->next != nullptr) p = p->next;
+    p->next = term;
+}
